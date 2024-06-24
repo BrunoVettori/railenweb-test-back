2338860cdb95ed3763453f351d086d1b05e0b8a3
BrunoVettori <bruno_vettori@porton.me>
2024-06-24 20:39:24 -0300

    progress

--- src/Products.php
+++ src/Products.php
@@ -1,6 +1,9 @@
 <?php
 
 header("Content-type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header('Access-Control-Allow-Credentials: true');
 
 $uris = explode("/", $_SERVER["REQUEST_URI"]);
 
@@ -10,14 +13,74 @@ $query = $_SERVER['QUERY_STRING'];
 
 $body =  file_get_contents('php://input');
 
-if ($method =="GET"){
-    echo $method;
-}elseif($method == "POST"){
-    echo $method;
-}elseif($method == "PUT"){
-    echo $method;
-}elseif($method == "DELETE"){
-    echo $method;
+$host = "127.0.0.1";
+$user = "user";
+$pass = "password";
+$dbase = "mydatabase";
+$dsn = "mysql:host=$host;dbname=$dbase";
+
+try {
+    $pdo = new PDO($dsn, $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo $e;
 }
 
-// echo $uris[3];
+if ($method == "GET") {
+
+    $sth = $pdo->prepare('SELECT * FROM produtos');
+
+    $sth->execute();
+
+    $result = $sth->fetchall();
+
+    echo "[";
+
+    for ($i = 0; $i < count($result); $i++) {
+
+        $myObjet = new stdClass();
+
+        $myObjet->id = $result[$i][0];
+        $myObjet->codigo = $result[$i][1];
+        $myObjet->nome = $result[$i][2];
+        $myObjet->descricao = $result[$i][3];
+        $myObjet->imagem = $result[$i][4];
+        $myObjet->valor = $result[$i][5];
+
+        $myJSON = json_encode($myObjet);
+
+        echo $myJSON;
+
+        if ($i != count($result) - 1) {
+            echo ",";
+        }
+    }
+
+    echo "]";
+} elseif ($method == "POST") {
+
+    $data = json_decode($body);
+
+    $sth = $pdo->prepare("INSERT INTO produtos (codigo, nome, descricao, valor) VALUES ('$data->codigo', '$data->nome', '$data->descricao', '$data->valor');");
+
+    $sth->execute();
+
+    $result = $sth->fetchall();
+
+    echo "sucesso";
+
+} elseif ($method == "PUT") {
+    echo $method;
+} elseif ($method == "DELETE") {
+
+    $data = json_decode($body);
+
+    $sth = $pdo->prepare(" DELETE FROM produtos WHERE id = $data->id;");
+
+    $sth->execute();
+
+    $result = $sth->fetchall();
+
+    echo "sucesso";
+    
+}
--- src/Users.php
+++ src/Users.php
@@ -2,6 +2,9 @@
 
 header("Content-type: application/json; charset=UTF-8");
 
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Credentials: true');
+
 $uris = explode("/", $_SERVER["REQUEST_URI"]);
 
 $method = $_SERVER["REQUEST_METHOD"];
@@ -10,14 +13,57 @@ $query = $_SERVER['QUERY_STRING'];
 
 $body =  file_get_contents('php://input');
 
-if ($method =="GET"){
-    echo $method;
-}elseif($method == "POST"){
-    echo $method;
-}elseif($method == "PUT"){
-    echo $method;
-}elseif($method == "DELETE"){
-    echo $method;
+// if ($method == "GET") {
+//     echo $method;
+// } elseif ($method == "POST") {
+//     echo $method;
+// } elseif ($method == "PUT") {
+//     echo $method;
+// } elseif ($method == "DELETE") {
+//     echo $method;
+// }
+
+$host = "127.0.0.1";
+$user = "user";
+$pass = "password";
+$dbase = "mydatabase";
+$dsn = "mysql:host=$host;dbname=$dbase";
+
+try {
+    $pdo = new PDO($dsn, $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo $e;
 }
 
-// echo $uris[3];
+$sth = $pdo->prepare('SELECT * FROM produtos');
+
+$sth->execute();
+
+$result = $sth->fetchall();
+
+echo "[";
+
+for ($i = 0; $i < count($result); $i++) {
+
+
+    $myObjet = new stdClass();
+
+    $myObjet->id = $result[$i][0];
+    $myObjet->codigo=$result[$i][1];
+    $myObjet->nome=$result[$i][2];
+    $myObjet->descricao=$result[$i][3];
+    $myObjet->imagem=$result[$i][4];
+    $myObjet->valor=$result[$i][5];
+    
+    $myJSON = json_encode($myObjet);
+
+    echo $myJSON;
+
+    if($i != count($result)-1){
+        echo ",";
+    }
+
+}
+
+echo "]";
